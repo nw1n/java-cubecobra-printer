17b60f728025bcc7418e7f14b2a85a929a27667f
nw1n <your@example.com>
2024-02-19 11:45:59 +0100

    clean

--- src/main/java/org/example/FileDownloader.java
+++ src/main/java/org/example/FileDownloader.java
@@ -12,9 +12,6 @@ import javax.imageio.ImageIO;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 
 public class FileDownloader {
