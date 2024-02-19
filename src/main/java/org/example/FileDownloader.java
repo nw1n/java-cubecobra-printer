0eadae41a38ee389011d7b65f20b8a163e620ab4
nw1n <your@example.com>
2024-02-19 11:43:23 +0100

    Add borders and triangles to image

--- src/main/java/org/example/FileDownloader.java
+++ src/main/java/org/example/FileDownloader.java
@@ -49,27 +49,57 @@ public class FileDownloader {
         File imageFile = new File(localFilePath);
         BufferedImage originalImage = ImageIO.read(imageFile);
 
-        // Create a graphics object to draw on the original image
-        Graphics2D g2d = originalImage.createGraphics();
+        // Desired border width in pixels
+        int borderWidth = 24;
+        int triangleSize = 72;
+        Color borderColor = new Color(24, 21, 16, 255);
+
+        // Create a new BufferedImage with increased dimensions to accommodate the border
+        int newWidth = originalImage.getWidth() + 2 * borderWidth;
+        int newHeight = originalImage.getHeight() + 2 * borderWidth;
+        BufferedImage borderedImage = new BufferedImage(newWidth, newHeight, originalImage.getType());
+
+        // Create graphics object to draw on the bordered image
+        Graphics2D g2d = borderedImage.createGraphics();
+
+        // Set the background color (in this case, white)
+        g2d.setColor(borderColor);
+        g2d.fillRect(0, 0, newWidth, newHeight);
+
+        // Draw the original image onto the bordered image with an offset to account for the border
+        int x = borderWidth;
+        int y = borderWidth;
+        g2d.drawImage(originalImage, x, y, null);
+
+        // Set the color of the borders (in this case, black)
+        g2d.setColor(borderColor);
+
+        // Draw the top border
+        g2d.fillRect(0, 0, newWidth, borderWidth);
+        // Draw the bottom border
+        g2d.fillRect(0, newHeight - borderWidth, newWidth, borderWidth);
+        // Draw the left border
+        g2d.fillRect(0, borderWidth, borderWidth, originalImage.getHeight());
+        // Draw the right border
+        g2d.fillRect(newWidth - borderWidth, borderWidth, borderWidth, originalImage.getHeight());
 
         // Set the color of the triangles (in this case, black)
-        g2d.setColor(new Color(24, 21, 16, 255));
+        g2d.setColor(borderColor);
 
         // Draw triangles at each corner
-        int triangleSize = 80; // Size of the triangles
         g2d.fillPolygon(new int[]{0, triangleSize, 0}, new int[]{0, 0, triangleSize}, 3); // Top-left corner
-        g2d.fillPolygon(new int[]{originalImage.getWidth() - triangleSize, originalImage.getWidth(), originalImage.getWidth()}, new int[]{0, 0, triangleSize}, 3); // Top-right corner
-        g2d.fillPolygon(new int[]{0, triangleSize, 0}, new int[]{originalImage.getHeight(), originalImage.getHeight(), originalImage.getHeight() - triangleSize}, 3); // Bottom-left corner
-        g2d.fillPolygon(new int[]{originalImage.getWidth() - triangleSize, originalImage.getWidth(), originalImage.getWidth()}, new int[]{originalImage.getHeight(), originalImage.getHeight(), originalImage.getHeight() - triangleSize}, 3); // Bottom-right corner
+        g2d.fillPolygon(new int[]{newWidth - triangleSize, newWidth, newWidth}, new int[]{0, 0, triangleSize}, 3); // Top-right corner
+        g2d.fillPolygon(new int[]{0, triangleSize, 0}, new int[]{newHeight, newHeight, newHeight - triangleSize}, 3); // Bottom-left corner
+        g2d.fillPolygon(new int[]{newWidth - triangleSize, newWidth, newWidth}, new int[]{newHeight, newHeight, newHeight - triangleSize}, 3); // Bottom-right corner
 
         // Dispose of the graphics object to free up resources
         g2d.dispose();
 
-        // Save the image with triangles
-        File outputFile = new File(localFilePath.replace(".png", "_bordered.png"));
-        ImageIO.write(originalImage, "png", outputFile);
+        // Save the image with borders and triangles
+        File outputFile = new File(localFilePath.replace(".png", "_bordered_triangles.png"));
+        ImageIO.write(borderedImage, "png", outputFile);
 
-        System.out.println("Triangles added successfully!");
+        System.out.println("Borders and triangles added successfully!");
     }
 
 
